d07290193c2f7401f2fa4c4d9785d31f0137ef12
Isurulak1 <isurulakshan0417@gmail.com>
2024-08-31 22:58:01 +0530

    Login sign up created

---
+++ src/config/firebase.js
@@ -0,0 +1,61 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword,getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { doc,getFirestore, setDoc } from "firebase/firestore";
+import {toast} from "react-toastify";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRBf9w_DaSKVfX4Rppa6ddj9G_eOkL2aY",
+  authDomain: "chat-app-gs-161e5.firebaseapp.com",
+  projectId: "chat-app-gs-161e5",
+  storageBucket: "chat-app-gs-161e5.appspot.com",
+  messagingSenderId: "1021246628011",
+  appId: "1:1021246628011:web:476efaf6545bea90cc97ee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (username,email,password) => {
+  try{
+    const res = await createUserWithEmailAndPassword(auth,email,password)
+    const user = res.user;
+    await setDoc(doc(db,"users",user.uid),{
+      id:user.uid,
+      username:username.toLowerCase(),
+      email,
+      name:"",
+      avatar:"",
+      bio:"Hey,There i am using chat app",
+      lastSeen:Date.now()
+    })
+    await setDoc(doc(db,"chats",user.uid),{
+      chatData:[]
+    })
+  }catch(error){
+    console.error(error)
+    toast.error(error.code.split('/')[1].split('-').join(" "));
+  }
+}
+
+const login = async(email,password) => {
+  try{
+    await signInWithEmailAndPassword(auth,email,password)
+  } catch (error) {
+    console.error(error);
+    toast.error(error.code.split('/')[1].split('-').join(" "));
+  }
+}
+
+const logout = async () => {
+  try{
+    await signOut(auth)
+  } catch (error) {
+    console.error(error)
+    toast.error(error.code.split('/')[1].split('-').join(" "));
+  }
+  await signOut(auth)
+}
+
+export{signup,login,logout,auth,db}
--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth,db } from '../../config/firebase';
+
 
 const ProfileUpdate = () => {
+
+  const[image,setImage] = useState(false);
+  const [name,setName] = useState("");
+  const [bio,setBio] = useState("");
+  const [uid,setUid] = useState("");
+
+  useEffect(()=>{
+    onAuthStateChanged(auth,async (user)=>{
+      if (user) {
+        setUid(user.uid)
+        const docRef = doc(db,"users",user.uid);
+      }
+    })
+  },[])
+
   return (
-    <div>ProfileUpdate</div>
+    <div className='profile'>
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor='avatar'>
+            <input onChange={(e)=>setImage(e.target.files[0])} type='file' id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+            <img src={image? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
+            Upload profile image
+          </label>
+          <input onChange={(e)=>setName(e.target.value)} value={name} type='text' placeholder='Your Name' required/>
+          <textarea onChange={(e)=>setBio(e.target.bio)} value={bio} placeholder='Write Profile bio' required></textarea>
+          <button type='submit'>Save</button>
+        </form>
+        <img className='profile-pic' src={image? URL.createObjectURL(image):assets.logo_icon} alt="" />
+      </div>
+    </div>
   )
 }
 
